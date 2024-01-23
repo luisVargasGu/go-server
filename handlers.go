e6f940c46a5b1387f708acad4b7279cffcb0818a
Luis Vargas <vargasluis012@gmail.com>
2024-01-22 23:06:05 -0500

    Formatting, and authHandler improvements.

--- handlers.go
+++ handlers.go
@@ -1,36 +1,41 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "os"
-    "io"
-    "database/sql"
-    _ "github.com/lib/pq" 
-    "fmt"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/lib/pq"
+	"io"
+	"net/http"
+	"os"
 )
 
 func ImageHandler(w http.ResponseWriter, r *http.Request) {
-    imageID := r.URL.Query().Get("id")
-    imagePath := "images/" + imageID + ".jpg"
+	imageID := r.URL.Query().Get("id")
+	imagePath := "images/" + imageID + ".jpg"
 
-    file, err := os.Open(imagePath)
-    if err != nil {
-        http.Error(w, "Image not Found", http.StatusNotFound)
-        return
-    }
-    defer file.Close()
+	file, err := os.Open(imagePath)
+	if err != nil {
+		http.Error(w, "Image not Found", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
 
-    _, err = io.Copy(w, file)
-    if err != nil {
-        http.Error(w, "Error serving image", http.StatusInternalServerError)
-        return
-    }
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, "Error serving image", http.StatusInternalServerError)
+		return
+	}
 }
 
 type Credentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+type AuthResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message.omitempty"`
+	UserID  int    `json:"user_id.omitempty"`
 }
 
 func AuthHandler(w http.ResponseWriter, r *http.Request) {
@@ -45,36 +50,51 @@ func AuthHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-
+        //TODO: check creds against database
 	if creds.Username == "admin" && creds.Password == "password" {
-		w.Write([]byte("true"))
+		response := AuthResponse{
+			Success: true,
+			Message: "Authentication successful",
+			UserID:  1, 
+		}
+		sendJSONResponse(w, http.StatusOK, response)
 	} else {
-		w.Write([]byte("false"))
+		response := AuthResponse{
+			Success: false,
+			Message: "Invalid credentials",
+		}
+		sendJSONResponse(w, http.StatusUnauthorized, response)
 	}
 }
 
+func sendJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
+
 func DbConnect() {
-    const (
-        host = "localhost"
-        port = 5432
-        user = "admin"
-        password = "password"
-        dbname = "chat_app"
-    )
+	const (
+		host     = "localhost"
+		port     = 5432
+		user     = "admin"
+		password = "password"
+		dbname   = "chat_app"
+	)
 
-    psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-                            host, port, user, password, dbname)
-    db, err := sql.Open("postgres", psqlconn)
-    if err != nil {    
-        panic(err)
-    }
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		panic(err)
+	}
 
-    defer db.Close()
+	defer db.Close()
 
-    err = db.Ping()
-    if err != nil {
-        panic(err)
-    }
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Println("Successfully connected!")
+	fmt.Println("Successfully connected!")
 }
