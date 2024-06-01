a4aceff0b11f44a3357813bbeb63268c280e1dc4
Luis Vargas <vargasluis012@gmail.com>
2024-06-01 18:36:33 -0400

    Updated utils to handle CORS better.

--- services/utils/utils.go
+++ services/utils/utils.go
@@ -36,12 +36,13 @@ func ParseJSON(r *http.Request, v any) error {
 func CorsHandler(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
+
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 		if r.Method == http.MethodOptions {
-			// Handle preflight request
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "POST")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.WriteHeader(http.StatusNoContent)
 			return
 		} else {
