f4c562e33f85eed846ca4fc0993ab4d231cd05b0
Luis Vargas <vargasluis012@gmail.com>
2024-06-03 09:35:06 -0400

    Removed unused functionto save messages.

--- services/message/routes.go
+++ services/message/routes.go
@@ -9,9 +9,7 @@ import (
 	"user/server/services/hub"
 	"user/server/services/utils"
 	"user/server/types"
-
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 )
 
 type Handler struct {
@@ -24,20 +22,11 @@ func NewHandler(store types.MessageStore, userStore types.UserStore) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(r *mux.Router) {
-	r.HandleFunc("/channels/{channelID}/room/{roomID}/messages", auth.WithJWTAuth(h.ChatAndCreateMessageHandler, h.userStore))
+	r.HandleFunc("/channels/{channelID}/room/{roomID}/messages", auth.WithJWTAuth(h.ChattingHandler, h.userStore))
 	r.HandleFunc("/rooms/{roomID}/messages", auth.WithJWTAuth(
 		utils.CorsHandler(h.FetchMessages),
 		h.userStore),
 	).Methods("GET")
-}
-
-func (h *Handler) ChatAndCreateMessageHandler(w http.ResponseWriter, r *http.Request) {
-	if websocket.IsWebSocketUpgrade(r) {
-		h.ChattingHandler(w, r)
-		return
-	}
-
-	h.CreateMessage(w, r)
 }
 
 func (h *Handler) ChattingHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,7 +56,7 @@ func (h *Handler) ChattingHandler(w http.ResponseWriter, r *http.Request) {
 	client := hub.NewClient(ws, fmt.Sprint(user.ID), fmt.Sprint(user.Username))
 	room.Register <- client
 
-	go client.ReadMessages(room)
+	go client.ReadMessages(room, h.store)
 
 	client.WriteMessages()
 }
@@ -90,30 +79,3 @@ func (h *Handler) FetchMessages(w http.ResponseWriter, r *http.Request) {
 	utils.SendJSONResponse(w, http.StatusOK, response)
 }
 
-func (h *Handler) CreateMessage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	channelID, err := strconv.Atoi(vars["channelID"])
-	roomID, err := strconv.Atoi(vars["roomID"])
-	if err != nil {
-		http.Error(w, "Invalid channel or room", http.StatusBadRequest)
-		log.Println("Invalid channel or room:", err)
-		return
-	}
-
-	channel := hub.HubInstance.Channels[channelID]
-	room := channel.Rooms[roomID]
-
-	user := r.Context().Value(auth.UserKey).(*types.User)
-
-	var message types.Message
-	err = utils.ParseJSON(r, &message)
-	if err != nil {
-		http.Error(w, "Invalid message", http.StatusBadRequest)
-		log.Println("Invalid message:", err)
-		return
-	}
-
-	message.SenderID = user.Username
-	message.RoomID = room.ID
-	room.Broadcast <- []byte(message.Content)
-}
