c8d27c9e742d7982bf2bf15bffc7211f501febc8
Luis Vargas <vargasluis012@gmail.com>
2024-06-08 13:02:23 -0400

    Moved Channel types to a separate file. Added mutex support.

---
+++ types/channel.go
@@ -0,0 +1,31 @@
+package types
+
+import "sync"
+
+type Channel struct {
+	mu    sync.RWMutex
+	ID    int           `json:"id"`
+	Name  string        `json:"name"`
+	Rooms map[int]*Room `json:"-"`
+}
+
+type ChannelStore interface {
+	GetAllChannels() ([]*Channel, error)
+	GetChannelsForUser(userID int) ([]*Channel, error)
+	CreateChannel(channel *Channel, user *User) error
+	DeleteChannel(channelID int) error
+}
+
+type ChannelResponse struct {
+	Channels []*Channel `json:"channels"`
+}
+
+func (c *Channel) GetRoom(roomID int) *Room {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	room, ok := c.Rooms[roomID]
+	if !ok {
+		return nil
+	}
+	return room
+}
