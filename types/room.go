bcd913223824590cb4544b4757f7812c846579e4
Luis Vargas <vargasluis012@gmail.com>
2024-06-09 14:09:10 -0400

    Added mutex support for rooms and the room Run handler.

--- types/room.go
+++ types/room.go
@@ -1,6 +1,9 @@
 package types
 
+import "sync"
+
 type Room struct {
+	mu         sync.RWMutex
 	ID         int              `json:"id"`
 	Name       string           `json:"name"`
 	ChannelID  int              `json:"channel_id"`
@@ -25,22 +28,32 @@ func (r *Room) Run() {
 	for {
 		select {
 		case client := <-r.Register:
+			r.mu.Lock()
 			r.Clients[client] = true
+			r.mu.Unlock()
 
 		case client := <-r.Unregister:
+			r.mu.Lock()
 			if _, ok := r.Clients[client]; ok {
 				delete(r.Clients, client)
 				close(client.Send)
 			}
+			r.mu.Unlock()
 		case message := <-r.Broadcast:
+			r.mu.RLock()
 			for client := range r.Clients {
 				select {
 				case client.Send <- message:
 				default:
+					r.mu.RUnlock()
+					r.mu.Lock()
 					delete(r.Clients, client)
 					close(client.Send)
+					r.mu.Unlock()
+					r.mu.RLock()
 				}
 			}
+			r.mu.RUnlock()
 		}
 	}
 }
