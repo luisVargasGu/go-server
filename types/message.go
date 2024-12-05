4d4c6f354de6fd226ad79b1cc7464a7d0c113656
Luis Vargas <vargasluis012@gmail.com>
2024-12-05 17:39:43 -0500

    Added more fields to message struct to handle all scenarios during peer connection.

--- types/message.go
+++ types/message.go
@@ -5,20 +5,27 @@ import (
 	"time"
 )
 
+// TODO: move ID, name and Avater to a separate Struct
+// TODO: move some fields to a different type so it's more performant
+// If they are always together
 type Message struct {
-	ID           int                        `json:"id,omitempty"`
-	Type         string                     `json:"type"`
-	RoomID       int                        `json:"room_id"`
-	SenderID     int                        `json:"sender_id"`
-	SenderName   string                     `json:"sender_name,omitempty"`
-	SenderAvatar string                     `json:"sender_avatar,omitempty"`
-	Content      string                     `json:"content,omitempty"`
-	SeenBy       []SeenByUser               `json:"seen_by,omitempty"`
-	Timestamp    time.Time                  `json:"timestamp,omitempty"`
-	IsRead       bool                       `json:"is_read"`
-	Offer        *webrtc.SessionDescription `json:"offer,omitempty"`
-	Answer       *webrtc.SessionDescription `json:"answer,omitempty"`
-	Candidate    *webrtc.ICECandidateInit   `json:"candidate,omitempty"`
+	ID              int                        `json:"id,omitempty"`
+	Type            string                     `json:"type"`
+	TrackID         string                     `json:"track_id,omitempty"`
+	RoomID          int                        `json:"room_id"`
+	SenderID        int                        `json:"sender_id"`
+	SenderName      string                     `json:"sender_name,omitempty"`
+	SenderAvatar    string                     `json:"sender_avatar,omitempty"`
+	Content         string                     `json:"content,omitempty"`
+	SeenBy          []SeenByUser               `json:"seen_by,omitempty"`
+	Timestamp       time.Time                  `json:"timestamp,omitempty"`
+	IsRead          bool                       `json:"is_read"`
+	IsVideoEnabled  *bool                      `json:"isVideoEnabled,omitempty"`
+	IsScreenEnabled *bool                      `json:"isScreenEnabled,omitempty"`
+	IsMicEnabled    *bool                      `json:"isMicEnabled,omitempty"`
+	Offer           *webrtc.SessionDescription `json:"offer,omitempty"`
+	Answer          *webrtc.SessionDescription `json:"answer,omitempty"`
+	Candidate       *webrtc.ICECandidateInit   `json:"candidate,omitempty"`
 }
 
 type MessageStore interface {
