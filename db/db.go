a8f128bf93364697917f49c03623f87c378475e7
Luis Vargas <vargasluis012@gmail.com>
2025-01-06 19:44:16 -0500

    Working db.

--- db/db.go
+++ db/db.go
@@ -3,22 +3,22 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"os"
-
 	_ "github.com/lib/pq"
+	"log"
 )
 
 var Db *sql.DB
 
 func DbConnect() *sql.DB {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	const (
+		host     = "localhost"
+		port     = 5432
+		user     = "admin"
+		password = "password"
+		dbname   = "chat_app"
+	)
 
-	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
